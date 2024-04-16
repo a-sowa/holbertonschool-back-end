69011bcc47d6d6c8f4fb98ec4d886c92d5ef2d5c
a-sowa <axelsowapro@gmail.com>
2024-04-16 19:29:06 +0000

    fix

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -8,41 +8,43 @@ import json
 import requests
 
 
-def get_todo_all_employees():
+def get_employee_todo_list_progress():
     """
-    Fetches TODO list data for all employees from a REST API
-    and exports the data to a JSON file.
+        Fetches TODO list data for all employees from a REST API
+        and exports the data to a JSON file.
 
-    Returns:
-        None
+        Returns:
+            None
     """
-    base_url = 'https://jsonplaceholder.typicode.com'
-    users_url = f'{base_url}/users'
-
-    users_response = requests.get(users_url)
+    base_url = "https://jsonplaceholder.typicode.com"
+    users_response = requests.get(f'{base_url}/users')
     users_data = users_response.json()
 
-    todo_all_employees = {}
+    all_tasks = {}
 
     for user in users_data:
-        user_id = user['id']
-        username = user['name']
-        todos_url = f'{base_url}/todos?userId={user_id}'
+        user_id = user.get('id')
+        user_name = user.get('username')
 
-        todos_response = requests.get(todos_url)
-        todos_data = todos_response.json()
+        todos_return = requests.get(f'{base_url}/todos?userId={user_id}')
+        todo_data = todos_return.json()
 
-        todo_all_employees[user_id] = [{'username':
-                                        username, 'task': todo['title'],
-                                        'completed': todo['completed']}
-                                       for todo in todos_data]
+        task_list = []
+        for task in todo_data:
+            task_title = task.get('title')
+            task_status = task.get('completed')
+            task_dict = {"username": user_name,
+                         "task": task_title,
+                         "completed": task_status
+                         }
+            task_list.append(task_dict)
 
-    filename = 'todo_all_employees.json'
-    with open(filename, 'w') as jsonfile:
-        json.dump(todo_all_employees, jsonfile, indent=4)
+            all_tasks[str(user_id)] = task_list
 
-    print(f'Data exported to {filename}')
+    json_filename = 'todo_all_employees.json'
+    with open(json_filename, mode='w') as json_file:
+        json.dump(all_tasks, json_file)
 
 
-if __name__ == "__main__":
-    get_todo_all_employees()
+if __name__ == '__main__':
+    get_employee_todo_list_progress()
